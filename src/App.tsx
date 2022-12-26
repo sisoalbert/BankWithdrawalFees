4a17e00d93c1363d5a785f42282c5248c3e64c8e
sisoalbert <sisoalbert@yahoo.com>
2022-12-26 21:57:59 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <img
+        alt="image"
+        src="/playground_assets/mix-900w.png"
+        className="home-image"
+      />
+    </div>
+  )
+}
+
+export default Home
