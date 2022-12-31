ac92ef8c1c04a8b012fc2f3df0b5f211ddb41183
sisoalbert <sisoalbert@yahoo.com>
2022-12-31 02:36:40 +0200

    added animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import './App.css'
+import {motion} from "framer-motion"
+
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div
+    className="home-container">
       <img
         alt="image"
         src="/playground_assets/mix-900w.png"
         className="home-image"
       />
+
+    <motion.div     initial ={{
+
+x:"-100vw"
+
+}}
+
+animate={{
+x:0
+}}
+
+transition={{
+  delay:1.5,
+  type:"spring",
+
+}}
+>
+    <a     
+    href="https://github.com/sisoalbert/BankWithdrawalFees">Visit the github repo!</a>
+      </motion.div>  
     </div>
   )
 }
